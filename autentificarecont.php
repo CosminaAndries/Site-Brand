f9f3f6a5ebbed663534f0731bccc00869ee05e30
CosminaAndries <162496999+CosminaAndries@users.noreply.github.com>
2025-07-01 22:51:45 +0300

    Add files via upload

---
+++ autentificarecont.php
@@ -0,0 +1,25 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$email=$_POST["email_box"];
+$parola=$_POST["password_box"];
+
+$db=mysqli_connect("localhost","root","", "datecont");
+if(!$db){
+exit("Error with the connection! " .mysqli_connect_error());	
+}
+else{
+	$interogare="SELECT * FROM creearecont WHERE  gmail='$email' AND parola='$parola'";
+	$rezultat_interogare=mysqli_query($db , $interogare );	
+	if(mysqli_num_rows($rezultat_interogare)>0){
+		
+		echo "Authetification done well!";
+		      mysqli_close($db);
+	}
+		else{
+			echo "Authentification didn't work!";
+ mysqli_close(
+ $db);			
+			}
+}
+}
+?>
